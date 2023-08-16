be80b1018eaf51f26f1d00a81d7620dc5914a626
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2023-08-16 09:41:02 +0900

    setting starter

---
+++ gongmoja/src/main/java/com/est/gongmoja/GongmojaApplication.java
@@ -0,0 +1,13 @@
+package com.est.gongmoja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GongmojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GongmojaApplication.class, args);
+	}
+
+}
---
+++ gongmoja/src/test/java/com/est/gongmoja/GongmojaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.est.gongmoja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GongmojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
