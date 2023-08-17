48f7319e66754a9fd43ded74450f8bd601207658
Minsoo Yeo <msyeo1404@gmail.com>
2023-08-17 15:01:55 +0900

    feat : Added ChatService with createChatRoom and joinChat

---
+++ gongmoja/src/main/java/com/est/gongmoja/dto/ChatRoomResponseDto.java
@@ -0,0 +1,12 @@
+package com.est.gongmoja.dto;
+
+import java.time.LocalDateTime;
+
+public class ChatRoomResponseDto {
+
+    private long id;
+    private long stockId;
+    private LocalDateTime openDate;
+    private LocalDateTime closeDate;
+
+}
