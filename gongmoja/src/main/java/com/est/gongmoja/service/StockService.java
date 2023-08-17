b1e9ed8a041e8a2853f71382f1d36f51c5a0d0b1
Minsoo Yeo <msyeo1404@gmail.com>
2023-08-17 10:33:19 +0900

    feat : Added UserService and StockService

---
+++ gongmoja/src/main/java/com/est/gongmoja/service/StockService.java
@@ -0,0 +1,11 @@
+package com.est.gongmoja.service;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@AllArgsConstructor
+public class StockService {
+}
