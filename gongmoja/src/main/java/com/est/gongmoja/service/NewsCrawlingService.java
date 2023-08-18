6f4bfba1305338e9eec60ee4a2a440cd7e7233b6
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2023-08-18 15:40:08 +0900

    feat: NewsCrawlingData db에 저장

--- gongmoja/src/main/java/com/est/gongmoja/service/NewsCrawlingService.java
+++ gongmoja/src/main/java/com/est/gongmoja/service/NewsCrawlingService.java
@@ -1,12 +1,28 @@
 package com.est.gongmoja.service;
 
+import com.est.gongmoja.entity.NewsEntity;
+import com.est.gongmoja.repository.NewsRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class NewsCrawlingService {
-    public static void main(String[] args) {
+    private final NewsRepository newsRepository;
+
+    @PostConstruct
+    public void getCrawlNewsData() throws IOException {
         String stockName = "대신밸런스제15호스팩";
         // 대상 웹 페이지 URL, 어디까지 크롤링할 지 정해야함.
         String url = "https://search.naver.com/search.naver?where=news&sm=tab_pge&query=" + stockName + "&sort=0&photo=0&field=0&pd=0&ds=&de=&cluster_rank=27&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so:r,p:all,a:all&start=1";
@@ -28,12 +44,15 @@ public class NewsCrawlingService {
                     String title = titleElement.text();
                     String summary = summaryElement.text();
                     String articleUrl = titleElement.attr("href");
-                    String timeInfo = timeElement.text();
+                    String timeInfo = timeElement.text().substring(0,timeElement.text().length() - 1);
                     String sourceName = sourceElement.text();
                     String imageUrl = imageElement.attr("data-lazysrc");
 
                     if (!imageUrl.startsWith("http"))
                         imageUrl = "https://www.google.com/s2/favicons?domain=example.com";
+
+                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd.HH.mm.ss");
+                    LocalDateTime publishedTime = LocalDateTime.parse(timeInfo + ".00.00.00", formatter);
 
                     System.out.println("Title: " + title);
                     System.out.println("Summary: " + summary);
@@ -43,8 +62,18 @@ public class NewsCrawlingService {
                     System.out.println("Image URL: " + imageUrl);
                     System.out.println();
                     //System.out.println(document);
+                    NewsEntity newsEntity = NewsEntity.builder()
+                            .newsUrl(url)
+                            .content(summary)
+                            .publishedTime(publishedTime)
+                            .imageUrl(imageUrl)
+                            .title(title)
+                            .publisher(sourceName)
+                            .build();
+                    newsRepository.save(newsEntity);
                 }
             }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
