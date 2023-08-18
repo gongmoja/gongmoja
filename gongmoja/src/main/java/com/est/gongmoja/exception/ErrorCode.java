bb75af36e41a196163f7eb4cfb4d5529538bf248
Yoon Jun Pil <galaxy160102@gmail.com>
2023-08-18 14:49:31 +0900

    build : exception 템플릿 추가

---
+++ gongmoja/src/main/java/com/est/gongmoja/exception/ErrorCode.java
@@ -0,0 +1,15 @@
+package com.est.gongmoja.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+
+    USERNAME_NOT_FOUND(HttpStatus.NOT_FOUND,"유저 정보가 존재하지 않습니다.");
+
+    private HttpStatus httpStatus;
+    private String message;
+}
