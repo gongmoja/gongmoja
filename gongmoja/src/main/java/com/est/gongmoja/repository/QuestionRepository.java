f2b553aaf03d7ea2557008ed4482e474054220a4
yudaeon <daeon03050@gmail.com>
2023-08-22 15:06:25 +0900

    refactor : Question 리팩토링 및 단일 조회 기능 추가

--- gongmoja/src/main/java/com/est/gongmoja/repository/QuestionRepository.java
+++ gongmoja/src/main/java/com/est/gongmoja/repository/QuestionRepository.java
@@ -2,12 +2,9 @@ package com.est.gongmoja.repository;
 
 import com.est.gongmoja.entity.QuestionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.ArrayList;
 import java.util.List;
 
-public interface QuestionRepository extends CrudRepository<QuestionEntity, Long> {
+public interface QuestionRepository extends JpaRepository<QuestionEntity, Long> {
     QuestionEntity findByTitle(String title);
 
     QuestionEntity findByTitleAndContent(String title, String content);
