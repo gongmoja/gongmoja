41dcaea4431c37fddb7cd659317d0c97a76dd08c
Minsoo Yeo <msyeo1404@gmail.com>
2023-08-25 13:45:13 +0900

    fix: allowedOrigins 에 충돌문제 해결

--- gongmoja/src/main/java/com/est/gongmoja/config/WebConfig.java
+++ gongmoja/src/main/java/com/est/gongmoja/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true)
