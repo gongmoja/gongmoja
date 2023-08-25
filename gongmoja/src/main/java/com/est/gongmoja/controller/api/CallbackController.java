5ecba1529721801d2b321797aee512151ea10549
Yoon Jun Pil <galaxy160102@gmail.com>
2023-08-25 18:06:18 +0900

    refactor : 쿠키 관련 로직 리팩터링

---
+++ gongmoja/src/main/java/com/est/gongmoja/controller/api/CallbackController.java
@@ -0,0 +1,14 @@
+package com.est.gongmoja.controller.api;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+public class CallbackController {
+
+
+
+}
--- gongmoja/src/main/java/com/est/gongmoja/oauth2/OAuth2SuccessHandler.java
+++ gongmoja/src/main/java/com/est/gongmoja/oauth2/OAuth2SuccessHandler.java
@@ -1,6 +1,7 @@
 package com.est.gongmoja.oauth2;
 
-import com.est.gongmoja.config.AppProperties;
+import
+        com.est.gongmoja.config.AppProperties;
 import com.est.gongmoja.entity.RefreshTokenEntity;
 import com.est.gongmoja.entity.UserEntity;
 import com.est.gongmoja.jwt.CookieUtil;
@@ -17,6 +18,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.oauth2.core.OAuth2RefreshToken;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
@@ -33,7 +35,6 @@ import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
-import static com.est.gongmoja.oauth2.HttpCookieOAuth2AuthorizationRequestRepository.REDIRECT_URI_PARAM_COOKIE_NAME;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -42,97 +43,18 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
     private final UserRepository userRepository;
     private final JwtTokenUtil jwtTokenUtil;
     private final RefreshTokenRepository refreshTokenRepository;
-    private final AppProperties appProperties;
-    private final HttpCookieOAuth2AuthorizationRequestRepository httpCookieOAuth2AuthorizationRequestRepository;
-
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         //OAuth 2 인증에 성공했을 경우 여기로 들어온다
         log.info("success 안에 들어옴");
-        OAuth2AccessToken accessToken;
         String targetUrl = determineTargetUrl(request, response, authentication);
 
-        if (response.isCommitted()) {
-            logger.debug("Response has already been committed. Unable to redirect to " + targetUrl);
-            return;
-        }
 
-        clearAuthenticationAttributes(request, response);
+
+
         getRedirectStrategy().sendRedirect(request, response, targetUrl);
-
-
-
-
-        //getRedirectStrategy().sendRedirect(request,response,"/");
     }
 
 
-
-
-
-
-
-
-
-
-    protected String determineTargetUrl(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-        Optional<String> redirectUri = CookieUtil.getCookie(request, REDIRECT_URI_PARAM_COOKIE_NAME)
-                .map(Cookie::getValue);
-
-        if(redirectUri.isPresent() && !isAuthorizedRedirectUri(redirectUri.get())) {
-            throw new RuntimeException("Sorry! We've got an Unauthorized Redirect URI and can't proceed with the authentication");
-        }
-
-        String targetUrl = redirectUri.orElse(getDefaultTargetUrl());
-
-
-        CustomOAuth2User user = (CustomOAuth2User) authentication.getPrincipal();
-        String username = user.getEmail();
-
-        String accessToken = jwtTokenUtil.createToken(username,JwtTokenUtil.accessTokenExpireMs);
-
-        return UriComponentsBuilder.fromUriString(targetUrl)
-                .queryParam("token", accessToken)
-                .build().toUriString();
-    }
-
-
-
-
-
-
-
-
-
-
-    protected void clearAuthenticationAttributes(HttpServletRequest request, HttpServletResponse response) {
-        super.clearAuthenticationAttributes(request);
-        httpCookieOAuth2AuthorizationRequestRepository.removeAuthorizationRequestCookies(request, response);
-    }
-
-
-
-
-
-
-
-
-
-
-    private boolean isAuthorizedRedirectUri(String uri) {
-        URI clientRedirectUri = URI.create(uri);
-
-        return appProperties.getOauth2().getAuthorizedRedirectUris()
-                .stream()
-                .anyMatch(authorizedRedirectUri -> {
-                    // Only validate host and port. Let the clients use different paths if they want to
-                    URI authorizedURI = URI.create(authorizedRedirectUri);
-                    if(authorizedURI.getHost().equalsIgnoreCase(clientRedirectUri.getHost())
-                            && authorizedURI.getPort() == clientRedirectUri.getPort()) {
-                        return true;
-                    }
-                    return false;
-                });
-    }
 
 }
