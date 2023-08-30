2e84253372893f95315b2c844388508026d6d3d9
sunga0101 <59547021+sunga0101@users.noreply.github.com>
2023-08-30 17:00:04 +0900

    feat: calendar 일정 클릭시 페이지 이동 구현

--- gongmoja/src/main/java/com/est/gongmoja/service/CalendarService.java
+++ gongmoja/src/main/java/com/est/gongmoja/service/CalendarService.java
@@ -28,10 +28,12 @@ public class CalendarService {
             eventData.put("title", "[청약] "+stock.getName());
             eventData.put("start", stock.getStartDate());
             eventData.put("end", stock.getEndDate());
-            eventData.put("status", "청약");
+            eventData.put("status", stock.getId()); //extendedProps
+            eventData.put("type", "청약"); //extendedProps
             eventData.put("backgroundColor", "#FFE5F1");
             eventData.put("textColor","#000000");
             eventData.put("borderColor","rgba(0, 185, 186, 0)");
+            eventData.put("url","/stock/"+stock.getId());
             eventDataList.add(eventData);
 
             // 2.환불일 데이터
@@ -39,9 +41,11 @@ public class CalendarService {
             eventData.put("title", "[환불] "+stock.getName());
             eventData.put("start", stock.getRefundDate());
             eventData.put("end", stock.getRefundDate());
-            eventData.put("status", "환불");
+            eventData.put("status", stock.getId()); //extendedProps
+            eventData.put("type", "환불"); //extendedProps
             eventData.put("textColor","#FBF0B2");
             eventData.put("backgroundColor", "#CAEDFF");
+            eventData.put("url","/stock/"+stock.getId());
             eventDataList.add(eventData);
 
             // 3.상장일 데이터
@@ -49,9 +53,11 @@ public class CalendarService {
             eventData.put("title", "[상장] "+stock.getName());
             eventData.put("start", stock.getIpoDate());
             eventData.put("end", stock.getIpoDate());
-            eventData.put("status", "상장");
+            eventData.put("status", stock.getId()); //extendedProps
+            eventData.put("type", "상장"); //extendedProps
             eventData.put("textColor","#FBF0B2");
             eventData.put("backgroundColor", "#D8B4F8");
+            eventData.put("url","/stock/"+stock.getId());
             eventDataList.add(eventData);
         }
         return eventDataList;
