347c8c007c3d069219838f7447b88a84185a8377
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2023-08-31 18:06:38 +0900

    mypage에 user객체 넘기기

--- gongmoja/src/main/java/com/est/gongmoja/controller/StockController.java
+++ gongmoja/src/main/java/com/est/gongmoja/controller/StockController.java
@@ -1,8 +1,11 @@
 package com.est.gongmoja.controller;
 
 import com.est.gongmoja.entity.StockEntity;
+import com.est.gongmoja.entity.UserEntity;
 import com.est.gongmoja.service.StockService;
+import com.est.gongmoja.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,11 +17,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/stock")
 public class StockController {
     private final StockService stockService;
+    private final UserService userService;
 
     @GetMapping("/{stockId}")
-    public String displayStock(Model model, @PathVariable("stockId")Long stockId){
-        StockEntity stock = stockService.findStockById(stockId);
-        model.addAttribute("stock", stock);
-        return "stock/stock";
+        public String displayStock(Model model, @PathVariable("stockId")Long stockId, Authentication authentication){
+            UserEntity user = (UserEntity) authentication.getPrincipal();
+            UserEntity userEntity = userService.getUser(user.getUserName());
+            StockEntity stock = stockService.findStockById(stockId);
+            model.addAttribute("stock", stock);
+            model.addAttribute("userEntity", userEntity);
+            return "stock/stock";
     }
 }
