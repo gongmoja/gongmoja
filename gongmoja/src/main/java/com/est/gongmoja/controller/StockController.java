dcbe60f5927c763af27d07495049ab2db99a7e9c
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2023-09-04 14:06:43 +0900

    stock.html 뉴스 추가

--- gongmoja/src/main/java/com/est/gongmoja/controller/StockController.java
+++ gongmoja/src/main/java/com/est/gongmoja/controller/StockController.java
@@ -1,7 +1,9 @@
 package com.est.gongmoja.controller;
 
+import com.est.gongmoja.entity.NewsEntity;
 import com.est.gongmoja.entity.StockEntity;
 import com.est.gongmoja.entity.UserEntity;
+import com.est.gongmoja.service.NewsService;
 import com.est.gongmoja.service.StockService;
 import com.est.gongmoja.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -12,19 +14,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Controller
 @RequestMapping("/stock")
 public class StockController {
     private final StockService stockService;
     private final UserService userService;
+    private final NewsService newsService;
 
     @GetMapping("/{stockId}")
         public String displayStock(Model model, @PathVariable("stockId")Long stockId, Authentication authentication){
+            List<NewsEntity> newsList = newsService.findAllNews(stockId);
             UserEntity user = (UserEntity) authentication.getPrincipal();
             UserEntity userEntity = userService.getUser(user.getUserName());
             StockEntity stock = stockService.findStockById(stockId);
             model.addAttribute("stock", stock);
+            model.addAttribute("newsList", newsList);
             model.addAttribute("userEntity", userEntity);
             return "stock/stock";
     }
