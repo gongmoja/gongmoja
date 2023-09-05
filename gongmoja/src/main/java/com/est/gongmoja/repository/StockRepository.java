7f7761f2c5a52bac17576779488d05c50a270daf
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2023-09-05 14:10:27 +0900

    feat: index.html(청약 진행중,예정 공모주나오게 경쟁률, 뉴스기사) 구현

--- gongmoja/src/main/java/com/est/gongmoja/repository/StockRepository.java
+++ gongmoja/src/main/java/com/est/gongmoja/repository/StockRepository.java
@@ -3,6 +3,8 @@ package com.est.gongmoja.repository;
 import com.est.gongmoja.entity.StockEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 public interface StockRepository extends JpaRepository<StockEntity,Long> {
--- gongmoja/src/main/java/com/est/gongmoja/service/CrawlingService.java
+++ gongmoja/src/main/java/com/est/gongmoja/service/CrawlingService.java
@@ -132,7 +132,7 @@ public class CrawlingService {
                     ipoDate = LocalDateTime.parse(year + "." + strings.get(6).substring(0, dates[0].length() - 1) + ".09.00.00", formatter);
                 }
                 else { // 크롤링시 null값일 때
-//                  ipoDate = LocalDateTime.of(1, 1, 1, 0, 0);
+                    ipoDate = LocalDateTime.of(1, 1, 1, 0, 0);
                 }
 
                 // 공모가
