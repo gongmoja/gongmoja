fc12a83f801cae616a8c5d71303fe376fa33fec6
Yoon Jun Pil <galaxy160102@gmail.com>
2023-09-06 09:09:57 +0900

    feat : admin page 등

--- gongmoja/src/main/java/com/est/gongmoja/repository/RefreshTokenRepository.java
+++
@@ -1,7 +0,0 @@
-package com.est.gongmoja.repository;
-
-import com.est.gongmoja.entity.RefreshTokenEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RefreshTokenRepository extends JpaRepository<RefreshTokenEntity, String> {
-}
