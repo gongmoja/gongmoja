c7aa9a054c79b123b376378f0fa3fc67bb8f6d13
Yoon Jun Pil <galaxy160102@gmail.com>
2023-09-06 09:09:40 +0900

    feat : admin page 등

--- gongmoja/src/main/java/com/est/gongmoja/jwt/JwtTokenFilter.java
+++ gongmoja/src/main/java/com/est/gongmoja/jwt/JwtTokenFilter.java
@@ -1,13 +1,10 @@
 package com.est.gongmoja.jwt;
 
-import com.est.gongmoja.entity.RefreshTokenEntity;
 import com.est.gongmoja.entity.UserEntity;
 import com.est.gongmoja.exception.CustomException;
 import com.est.gongmoja.exception.ErrorCode;
-import com.est.gongmoja.repository.RefreshTokenRepository;
 import com.est.gongmoja.repository.UserRepository;
 import com.est.gongmoja.service.RedisService;
-import com.est.gongmoja.service.UserService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
@@ -15,7 +12,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -25,8 +21,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
