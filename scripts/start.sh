0d42ccff32837e3f57a5138d4bc3d72488dd7566
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2023-09-10 00:07:28 +0900

    start.sh 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -10,7 +10,7 @@ START_LOG="$ROOT_PATH/start.log"
 NOW=$(date +%c)
 
 echo "[$NOW] $JAR 복사" >> $START_LOG
-cp $ROOT_PATH/build/libs/gongmoja-1.0.0.jar $JAR
+cp $ROOT_PATH/build/libs/gongmoja-0.0.1-SNAPSHOT.jar $JAR
 
 echo "[$NOW] > $JAR 실행" >> $START_LOG
 nohup java -jar $JAR > $APP_LOG 2> $ERROR_LOG &
