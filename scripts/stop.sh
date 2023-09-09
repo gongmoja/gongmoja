d40fdc4d5f82d9f2457b5248261e1895da3c2048
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2023-09-10 00:04:03 +0900

    start.sh 수정

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -11,4 +11,4 @@ if [ -z "$SERVICE_PID" ]; then
             echo "서비스 종료 " >> $STOP_LOG
               kill "$SERVICE_PID"
                 # kill -9 $SERVICE_PID # 강제 종료를 하고 싶다면 이 명령어 사용
-fi                                                                                                 
+fi
